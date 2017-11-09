49f8fc309903501e4672576b1a99e7824329acd1
0v1se <dev@0v1se.com>
2017-11-09 19:20:07 +0300

    deploy

--- contracts/bitcoin/BitcoinToken.sol
+++ contracts/bitcoin/BitcoinToken.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.4.0;
 
 
-contract BitcoinToken {
-    function BitcoinToken(){
+import "../external/ExternalToken.sol";
 
+
+contract BitcoinToken is ExternalToken {
+    function checkMinter() internal {
+        require(msg.sender == 0xc66d094ed928f7840a6b0d373c1cd825c97e3c7c);
     }
 }
