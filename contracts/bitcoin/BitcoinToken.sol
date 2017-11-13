dde548e1971b3ee146f876c35972aad38569b1f3
0v1se <dev@0v1se.com>
2017-11-13 10:18:03 +0300

    BitcoinToken tests

--- contracts/bitcoin/BitcoinToken.sol
+++ contracts/bitcoin/BitcoinToken.sol
@@ -1,11 +1,23 @@
 pragma solidity ^0.4.0;
 
 
+import "../upgradeable/Upgradeable.sol";
 import "../external/ExternalToken.sol";
 
 
-contract BitcoinToken is ExternalToken {
+contract BitcoinToken is Upgradeable, ExternalToken {
+    string public constant name = "Bitcoin";
+    string public constant symbol = "BTCT";
+    uint8 public constant decimals = 8;
+
+    address public constant admin = 0x20bf07925c8987beb981b1713420a219dcf85817;
+    address public constant minter = 0x8606e71586aac6361ce1adc05ca337ac38608781;
+
     function checkMinter() internal {
-        require(msg.sender == 0xc66d094ed928f7840a6b0d373c1cd825c97e3c7c);
+        require(msg.sender == minter || msg.sender == admin);
+    }
+
+    function checkAdmin() internal {
+        require(msg.sender == admin);
     }
 }
--- contracts/upgradeable/Upgradeable.sol
+++ contracts/upgradeable/Upgradeable.sol
@@ -1,6 +1,39 @@
-pragma solidity 0.4.15;
+pragma solidity ^0.4.15;
 
 
 contract Upgradeable {
     address public target;
+    event Upgrade(address target, address admin);
+
+    modifier onlyAdmin() {
+        checkAdmin();
+        _;
+    }
+
+    function checkAdmin() internal;
+
+    function upgrade(address _target) onlyAdmin public {
+        verifyTargetState(_target);
+        verifyState(_target);
+        target = _target;
+        Upgrade(_target, msg.sender);
+    }
+
+    function verifyTargetState(address testTarget) private {
+        require(address(delegateGet(testTarget, "target()")) == target);
+    }
+
+    function verifyState(address testTarget) internal {
+
+    }
+
+    function delegateGet(address testTarget, string signature) internal returns (bytes32 result) {
+        bytes4 targetCall = bytes4(keccak256(signature));
+        assembly {
+            let free := mload(0x40)
+            mstore(free, targetCall)
+            let retVal := delegatecall(gas, testTarget, free, 4, free, 32)
+            result := mload(free)
+        }
+    }
 }
