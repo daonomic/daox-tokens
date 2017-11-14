3c9347c0f1504622e21293f58cf6872ca51270e6
0v1se <dev@0v1se>
2017-11-14 11:19:26 +0300

    new minter, admin address

--- contracts/bitcoin/BitcoinToken.sol
+++ contracts/bitcoin/BitcoinToken.sol
@@ -10,11 +10,12 @@ contract BitcoinToken is Upgradeable, ExternalToken {
     string public constant symbol = "BTCT";
     uint8 public constant decimals = 8;
 
-    address public constant admin = 0x20bF07925c8987BeB981b1713420a219dCF85817;
-    address public constant minter = 0x8606E71586AAc6361ce1aDC05Ca337ac38608781;
+    address public constant admin = 0x10a44fF9805c23f559d9c9f783091398CE54A556;
+    address public constant minter1 = 0x884FFccB29d5aba8c94509663595F1dBF823dCC9;
+    address public constant minter2 = 0x5aCC33B4318575581a80522B2e57D1d09e5eC111;
 
     function checkMinter() internal {
-        require(msg.sender == minter || msg.sender == admin);
+        require(msg.sender == minter1 || msg.sender == minter2);
     }
 
     function checkAdmin() internal {
